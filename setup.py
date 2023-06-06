08eb8a1aaa0cb454f30fa5d427213149a65da3a0
dusklight00 <r.rahul.developer@gmail.com>
2023-06-06 12:26:55 +0530

    added link back to the documentation in pypi #3

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name="scrapgpt",
-    version="1.0.0",
+    version="1.1.0",
     description="Python library for scraping ChatGPT",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Dusk Light",
     packages=["scrapgpt"],
     zip_safe=False,
+    project_urls={
+        "Documentation": "https://github.com/dusklight00/scrapgpt",
+    },
     install_requires=[
         "webscapy"
     ]
